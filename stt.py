9fd3a9889964b62593b1fd7472e1343b774ed424
KHIWQQ <eiq1627@gmail.com>
2023-08-30 10:18:54 +0700

    stt

--- stt.py
+++ stt.py
@@ -23,14 +23,14 @@ with sr.Microphone(device_index=0) as source:
     text = r.recognize_google(audio, language='th-TH')
     print("You said: ", text)
 
-# tts=gTTS(text='เติมน้ำมัน'+ text +'เรียบร้อย',lang='th')
-# tts.save('Hello.mp3')
+tts=gTTS(text='เติมน้ำมัน'+ text +'เรียบร้อย',lang='th')
+tts.save('Hello.mp3')
 
-# file = "AskforOil.mp3"
-# print("Play mp3")
-# os.system("mpg123 "+file)
-# reduce noise 
-# r.adjust_for_ambient_noise(source)
+file = "AskforOil.mp3"
+print("Play mp3")
+os.system("mpg123 "+file)
+reduce noise 
+r.adjust_for_ambient_noise(source)
 
 # device = ['HDA Intel PCH: CX8200 Analog (hw:0,0)', 'HDA Intel PCH: HDMI 0 (hw:0,3)', 'HDA Intel PCH: HDMI 1 (hw:0,7)', 'HDA Intel PCH: HDMI 2 (hw:0,8)', 'HDA Intel PCH: HDMI 3 (hw:0,9)', 'HDA Intel PCH: HDMI 4 (hw:0,10)', 'sysdefault', 'front', 'surround40', 'surround51', 'surround71', 'hdmi', 'samplerate', 'speexrate', 'pulse', 'upmix', 'vdownmix', 'dmix', 'default']
 # print(device[0])
